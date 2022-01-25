e483aa60868e7820ffd073d876976ac7c4cbaae9
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2022-01-25 13:02:20 +0100

    everything finally works again

---
+++ screens/CameraScreen.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect, useRef } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Camera } from "expo-camera";
+import * as ImageManipulator from "expo-image-manipulator";
+
+export default function CameraScreen() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const cameraRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <>
+      <Camera style={styles.camera} ref={cameraRef} />
+      <Button
+        title="Take a picture"
+        onPress={async () => {
+          const pictureMetadata = await cameraRef.current.takePictureAsync();
+          console.log("pictureMetadata", pictureMetadata);
+          console.log(
+            await ImageManipulator.manipulateAsync(pictureMetadata.uri, [
+              { resize: { width: 800 } },
+            ])
+          );
+        }}
+      />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  camera: {
+    flex: 1,
+  },
+});
---
+++ screens/ImagesScreen.js
@@ -0,0 +1,66 @@
+import * as FileSystem from "expo-file-system";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Image, FlatList, Button } from "react-native";
+import singleFileUploader from "single-file-uploader";
+
+export default function ImagesScreen() {
+  const [imagesURI, setImagesURI] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const images = await FileSystem.readDirectoryAsync(
+        FileSystem.cacheDirectory + "ImageManipulator"
+      );
+      setImagesURI(images);
+    })();
+  }, []);
+  return imagesURI.length > 0 ? (
+    <FlatList
+      data={imagesURI}
+      keyExtractor={(imageURI) => imageURI}
+      renderItem={(itemData) => {
+        console.log("item", itemData);
+        return (
+          <>
+            <Image
+              style={styles.image}
+              source={{
+                uri:
+                  FileSystem.cacheDirectory +
+                  "ImageManipulator/" +
+                  itemData.item,
+              }}
+            />
+            <Button
+              title="upload"
+              onPress={async () => {
+                try {
+                  await singleFileUploader({
+                    distantUrl:
+                      "https://wildstagram.nausicaa.wilders.dev/upload",
+                    filename: itemData.item,
+                    filetype: "image/jpeg",
+                    formDataName: "fileData",
+                    localUri:
+                      FileSystem.cacheDirectory +
+                      "ImageManipulator/" +
+                      itemData.item,
+                  });
+                  alert("Uploaded");
+                } catch (err) {
+                  alert("Error");
+                }
+              }}
+            />
+          </>
+        );
+      }}
+    />
+  ) : null;
+}
+
+const styles = StyleSheet.create({
+  image: {
+    resizeMode: "cover",
+    height: 500,
+  },
+});
